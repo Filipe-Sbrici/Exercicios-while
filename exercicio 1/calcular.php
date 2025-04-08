9ac75cf12d914d5c1941d034f9b77b3ed16086c0
IsabelyFaria <bellyfaria04@gmail.com>
2025-04-08 11:37:44 -0300

    f  0.5

---
+++ exercicio 1/calcular.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 10</title>
+    <link href="styles.css" rel="stylesheet" >
+</head>
+<body>
+<?php
+
+    $num = $_POST ["num"];
+    $num100 = $num / 100;
+    $num50 = $num / 50;
+    $num20 = $num / 20;
+    $num10 = $num / 10;
+    $num5 = $num / 5;
+    $num2 = $num / 2;
+    
+    echo "Você precisaria de $num100 notas de R$100 para bater o número $num";
+    echo "Você precisaria de $num50 notas de R$50 para bater o número $num";
+    echo "Você precisaria de $num20 notas de R$20 para bater o número $num";
+    echo "Você precisaria de $num10 notas de R$10 para bater o número $num";
+    echo "Você precisaria de $num5 notas de R$5 para bater o número $num";
+    echo "Você precisaria de $num2 notas de R$2 para bater o número $num";
+
+?>
+</body>
+</html>
---
+++ exercicio 1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1</title>
+</head>
+<body>
+    
+<h1>CALCULADORA DE QUANTIDADE DE CÉDULAS</h1>
+<form method = "post" action="calcular.php">
+
+<h2>Insira o valor monetário:</h2>
+<input type = "number"  name="num" size="25" min="1"/> <br></br>
+
+<input type="submit">
+
+    
+</body>
+</html>
